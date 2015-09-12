68bd89e60bbb75661aa543dbf767bf25e8a1d8a4
dogdog7788 <dogdog7788@qq.com>
2015-09-12 15:19:42 +0800

    新加一个组装string数据的方法

--- src/main/java/com/sr178/common/jdbc/util/SqlUtil.java
+++ src/main/java/com/sr178/common/jdbc/util/SqlUtil.java
@@ -346,5 +346,24 @@ public class SqlUtil {
 
 		return beansSql;
 	}
+	
+	public static String joinStr(List<String> lists){
+		StringBuffer result = new StringBuffer();
+		if(lists!=null&&lists.size()>0){
+			for(int i=0;i<lists.size();i++){
+				String str = lists.get(i);
+				if(i==0){
+					result.append("'");
+					result.append(str);
+					result.append("'");
+				}else{
+					result.append(",'");
+					result.append(str);
+					result.append("'");
+				}
+			}
+		}
+		return result.toString();
+	}
 
 }
